1e843d9390522a49f2a8d5d63a7361e64fb2c44a
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-05-24 15:12:36 +0200

    Simple console game, where player must guess which number is generated by app.

---
+++ src/pl/pyt0/workshops/guessNumber.java
@@ -0,0 +1,38 @@
+package pl.pyt0.workshops;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNumber {
+
+    public static void main(String[] args) {
+        int userNum = 0;
+        int genNum = 0;
+        int countTries = 0;
+        checker(userNum, genNum, countTries);
+
+    }
+
+    public static void checker(int userNumber, int genNumber, int counter) {
+        Random random = new Random();
+        genNumber = random.nextInt(100);
+        System.out.println("Zgadnij liczbę z zakresu 1-100: ");
+        try {
+            while (userNumber != genNumber) {
+                counter++;
+                Scanner scanner = new Scanner(System.in);
+                userNumber = scanner.nextInt();
+                if (userNumber > genNumber) {
+                    System.out.println("Podana liczba jest za duża! Spróbuj ponownie! ");
+                } else if (userNumber < genNumber) {
+                    System.out.println("Podana liczba jest za małą! Spróbuj ponownie! ");
+                } else {
+                    System.out.println("Brawo! Zgadłeś za " + counter + " razem!");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Błąd, to nie jest liczba. Spróbuj ponownie!");
+        }
+    }
+}
