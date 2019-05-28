8319a2aee31328af08a7c9ece7981a9020dae65b
Bartłomiej Pietrzyk <bapietrzyk@gmail.com>
2019-05-28 10:15:16 +0200

    Guess number game ver.0

--- src/pl/pyt0/workshops/GuessNumber.java
+++ src/pl/pyt0/workshops/GuessNumber.java
@@ -4,20 +4,21 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-public class guessNumber {
+public class GuessNumber {
 
     public static void main(String[] args) {
         int userNum = 0;
         int genNum = 0;
         int countTries = 0;
         checker(userNum, genNum, countTries);
-
     }
 
     public static void checker(int userNumber, int genNumber, int counter) {
         Random random = new Random();
+
         genNumber = random.nextInt(100);
         System.out.println("Zgadnij liczbę z zakresu 1-100: ");
+
         try {
             while (userNumber != genNumber) {
                 counter++;
@@ -26,11 +27,13 @@ public class guessNumber {
                 if (userNumber > genNumber) {
                     System.out.println("Podana liczba jest za duża! Spróbuj ponownie! ");
                 } else if (userNumber < genNumber) {
-                    System.out.println("Podana liczba jest za małą! Spróbuj ponownie! ");
+                    System.out.println("Podana liczba jest za mała! Spróbuj ponownie! ");
                 } else {
                     System.out.println("Brawo! Zgadłeś za " + counter + " razem!");
+                    continue;
                 }
             }
+
         } catch (InputMismatchException e) {
             System.err.println("Błąd, to nie jest liczba. Spróbuj ponownie!");
         }
